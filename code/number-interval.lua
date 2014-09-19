9a08b85a814a0abfa12a7ad3907fcba653ff9e13
drewsberry <drew@drewsilcock.co.uk>
2014-09-19 15:59:10 +0100

    Add Lua and UBASIC CHDK scripts for reference

---
+++ code/number-interval.lua
@@ -0,0 +1,15 @@
+--[[
+@title Intervalometer
+@param a = interval (sec)
+@default a 5
+@param b = number of photos
+@default -1
+--]]
+repeat
+    start = get_tick_count()
+	shoot()
+    sleep(a*1000 - (get_tick_count() - start))
+    b = b - 1
+until ( false or b = 0)
+
+
